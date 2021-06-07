65b375c21228ffa1f88cddb1ed2090847e66aee3
Aditya Wasan <aditya@navanatech.in>
2021-06-07 13:55:09 +0530

    app: do not launch a new coroutine for updating db
    We always want to DB job to complete first before we save the worker as logged in worker because if there is an issue where app couldn't save the user, the app will go into a crash flow and cannot be recovered without clearing app data
    
    Signed-off-by: Aditya Wasan <aditya@navanatech.in>

--- client/app/src/main/java/com/microsoft/research/karya/ui/onboarding/accesscode/AccessCodeViewModel.kt
+++ client/app/src/main/java/com/microsoft/research/karya/ui/onboarding/accesscode/AccessCodeViewModel.kt
@@ -6,9 +6,17 @@ import com.microsoft.research.karya.data.manager.AuthManager
 import com.microsoft.research.karya.data.model.karya.WorkerRecord
 import com.microsoft.research.karya.data.repo.WorkerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.withContext
 
 @HiltViewModel
 class AccessCodeViewModel
@@ -37,9 +45,9 @@ constructor(private val workerRepository: WorkerRepository, private val authMana
       .launchIn(viewModelScope)
   }
 
-  private fun createWorker(accessCode: String, workerRecord: WorkerRecord) {
-    val dbWorker = workerRecord.copy(accessCode = accessCode)
-
-    viewModelScope.launch { workerRepository.upsertWorker(dbWorker) }
-  }
+  private suspend fun createWorker(accessCode: String, workerRecord: WorkerRecord) =
+    withContext(Dispatchers.IO) {
+      val dbWorker = workerRecord.copy(accessCode = accessCode)
+      workerRepository.upsertWorker(dbWorker)
+    }
 }
