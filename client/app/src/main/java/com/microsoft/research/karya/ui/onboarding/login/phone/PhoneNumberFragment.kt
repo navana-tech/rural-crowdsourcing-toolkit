b12cc74c5d843abfd8ec5f5fd2a5572e59e64135
Anurag Shukla <shukla.anurag0006@gmail.com>
2021-06-11 12:07:09 +0530

    Update Phone number UI

--- client/app/src/main/java/com/microsoft/research/karya/ui/onboarding/login/phone/PhoneNumberFragment.kt
+++ client/app/src/main/java/com/microsoft/research/karya/ui/onboarding/login/phone/PhoneNumberFragment.kt
@@ -14,6 +14,8 @@ import com.microsoft.research.karya.ui.base.BaseFragment
 import com.microsoft.research.karya.utils.AppConstants.PHONE_NUMBER_LENGTH
 import com.microsoft.research.karya.utils.extensions.dataStore
 import com.microsoft.research.karya.utils.extensions.doOnlyOnce
+import com.microsoft.research.karya.utils.extensions.disable
+import com.microsoft.research.karya.utils.extensions.enable
 import com.microsoft.research.karya.utils.extensions.gone
 import com.microsoft.research.karya.utils.extensions.hideKeyboard
 import com.microsoft.research.karya.utils.extensions.invisible
@@ -55,16 +57,16 @@ class PhoneNumberFragment : BaseFragment(R.layout.fragment_phone_number) {
     with(binding) {
       phoneNumberEt.doAfterTextChanged { phoneNumber ->
         if (!phoneNumber.isNullOrEmpty() && phoneNumber.length == PHONE_NUMBER_LENGTH) {
-          phoneNumberNextIv.handlePhoneNumberReady()
+          phoneNumberNextIv.enable()
         } else {
-          phoneNumberNextIv.handlePhoneNumberNotReady()
+          phoneNumberNextIv.disable()
         }
       }
 
       phoneNumberNextIv.setOnClickListener { handleNextClick(phoneNumberEt.text.toString()) }
 
       appTb.setTitle(getString(R.string.s_phone_number_title))
-      appTb.setAssistantClickListener { assistant.playAssistantAudio(AssistantAudio.PHONE_NUMBER_PROMPT) }
+//      appTb.setAssistantClickListener { assistant.playAssistantAudio(AssistantAudio.PHONE_NUMBER_PROMPT) }
     }
   }
 
@@ -94,7 +96,7 @@ class PhoneNumberFragment : BaseFragment(R.layout.fragment_phone_number) {
   private fun showInitialUi() {
     with(binding) {
       failToSendOtpTv.gone()
-      phoneNumberNextIv.handlePhoneNumberNotReady()
+      phoneNumberNextIv.disable()
       hideLoading()
     }
   }
@@ -103,8 +105,8 @@ class PhoneNumberFragment : BaseFragment(R.layout.fragment_phone_number) {
     with(binding) {
       failToSendOtpTv.gone()
       showLoading()
-      phoneNumberNextIv.handlePhoneNumberNotReady()
-      phoneNumberNextIv.invisible()
+      phoneNumberNextIv.disable()
+      phoneNumberNextIv.gone()
     }
   }
 
@@ -112,7 +114,7 @@ class PhoneNumberFragment : BaseFragment(R.layout.fragment_phone_number) {
     with(binding) {
       failToSendOtpTv.gone()
       hideLoading()
-      phoneNumberNextIv.handlePhoneNumberReady()
+      phoneNumberNextIv.enable()
     }
   }
 
@@ -121,25 +123,13 @@ class PhoneNumberFragment : BaseFragment(R.layout.fragment_phone_number) {
       failToSendOtpTv.text = message
       failToSendOtpTv.visible()
       hideLoading()
-      phoneNumberNextIv.handlePhoneNumberReady()
+      phoneNumberNextIv.enable()
     }
   }
 
   private fun handleNextClick(phoneNumber: String) {
     hideKeyboard()
     viewModel.sendOTP(phoneNumber)
-  }
-
-  private fun ImageView.handlePhoneNumberReady() {
-    setImageResource(0)
-    setImageResource(R.drawable.ic_next_enabled)
-    isClickable = true
-  }
-
-  private fun ImageView.handlePhoneNumberNotReady() {
-    setImageResource(0)
-    setImageResource(R.drawable.ic_next_disabled)
-    isClickable = false
   }
 
   private fun hideLoading() {
