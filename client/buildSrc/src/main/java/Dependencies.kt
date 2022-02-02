608cba7f3162a6260bb949bd1cec202eb361bbd5
Aditya Wasan <aditya@navanatech.in>
2022-02-02 12:56:49 +0530

    chore(buildSrc): update deps
    Signed-off-by: Aditya Wasan <aditya@navanatech.in>

--- client/buildSrc/src/main/java/Dependencies.kt
+++ client/buildSrc/src/main/java/Dependencies.kt
@@ -1,11 +1,11 @@
-private const val AGP_VERSION = "4.1.3"
-private const val ANDROIDX_HILT_VERSION = "1.0.0-beta01"
-private const val CRASHLYTICS_VERSION = "2.5.2"
-private const val DAGGER_HILT_VERSION = "2.34.1-beta"
-private const val GMS_VERSION = "4.3.5"
-private const val KOTLIN_VERSION = "1.4.30"
+private const val AGP_VERSION = "7.1.0"
+private const val ANDROIDX_HILT_VERSION = "1.0.0"
+private const val CRASHLYTICS_VERSION = "2.8.1"
+private const val DAGGER_HILT_VERSION = "2.40.5"
+private const val GMS_VERSION = "4.3.10"
+private const val KOTLIN_VERSION = "1.6.10"
 private const val LINT_VERSION = "27.3.0-alpha04"
-private const val NAVIGATION_VERSION = "2.3.5"
+private const val NAVIGATION_VERSION = "2.5.0-alpha01"
 
 object Plugins {
 
@@ -37,7 +37,6 @@ object Dependencies {
     const val appcompat = "androidx.appcompat:appcompat:1.3.0-rc01"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.0-beta01"
     const val fragmentKtx = "androidx.fragment:fragment-ktx:1.3.3"
-    const val legacy_support = "androidx.legacy:legacy-support-v4:1.0.0"
     const val work_runtime = "androidx.work:work-runtime-ktx:2.7.0-alpha03"
     const val multidex = "androidx.multidex:multidex:2.0.1"
     const val datastorePrefs = "androidx.datastore:datastore-preferences:1.0.0-beta01"
@@ -53,19 +52,18 @@ object Dependencies {
     }
 
     object Lifecycle {
-      private const val version = "2.4.0-alpha01"
+      private const val version = "2.5.0-alpha01"
       private const val extension_version = "2.2.0"
 
       const val common = "androidx.lifecycle:lifecycle-common-java8:$version"
       const val runtimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:$version"
-      const val extensions = "androidx.lifecycle:lifecycle-extensions:$extension_version"
       const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
       const val livedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
       const val saved_state = "androidx.lifecycle:lifecycle-viewmodel-savedstate:$version"
     }
 
     object Room {
-      private const val version = "2.4.0"
+      private const val version = "2.4.1"
 
       const val roomRuntime = "androidx.room:room-runtime:$version"
       const val roomCompiler = "androidx.room:room-compiler:$version"
@@ -81,11 +79,11 @@ object Dependencies {
 
   object Google {
 
-    const val gson = "com.google.code.gson:gson:2.8.6"
-    const val material = "com.google.android.material:material:1.4.0-alpha02"
+    const val gson = "com.google.code.gson:gson:2.8.9"
+    const val material = "com.google.android.material:material:1.6.0-alpha02"
 
     object Firebase {
-      const val bom = "com.google.firebase:firebase-bom:28.4.0"
+      const val bom = "com.google.firebase:firebase-bom:29.0.4"
       const val crashlytics = "com.google.firebase:firebase-crashlytics"
       const val analytics = "com.google.firebase:firebase-analytics"
     }
@@ -97,9 +95,9 @@ object Dependencies {
     const val debugDB = "com.amitshekhar.android:debug-db:1.0.6"
     const val glide = "com.github.bumptech.glide:glide:4.12.0"
     const val stateProgressBar = "com.kofigyan.stateprogressbar:stateprogressbar:1.0.0"
-    const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:4.9.1"
-    const val lottie = "com.airbnb.android:lottie:4.1.0"
-    const val okhttp = "com.squareup.okhttp3:okhttp:5.0.0-alpha.2"
+    const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:4.9.3"
+    const val lottie = "com.airbnb.android:lottie:4.2.1"
+    const val okhttp = "com.squareup.okhttp3:okhttp:5.0.0-alpha.3"
     const val zabaan = "in.navanatech:zabaan-sdk:1.2.2"
 
     object Retrofit {
