6c78532562525fd7b4b3978d90d33c20a7703856
Shreyansh Lodha <shreyansh@navanatech.in>
2022-10-17 20:16:50 +0530

    chore(VersionInterceptor): Adjust version information header based on backend requirement

--- client/app/src/main/java/com/microsoft/research/karya/data/remote/interceptors/VersionInterceptor.kt
+++ client/app/src/main/java/com/microsoft/research/karya/data/remote/interceptors/VersionInterceptor.kt
@@ -1,10 +1,11 @@
 package com.microsoft.research.karya.data.remote.interceptors
 
+import com.microsoft.research.karya.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class VersionInterceptor() : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
-    return chain.proceed(chain.request().newBuilder().addHeader("client-version-number", "5").build())
+    return chain.proceed(chain.request().newBuilder().addHeader("version", "v10").build())
   }
 }
